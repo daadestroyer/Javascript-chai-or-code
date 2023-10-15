d89145f9d9685d53e7f19f3bcaed737ca1f42c30
daadestroyer <nigamshubham2000@gmail.com>
2023-10-15 23:03:13 +0530

    datatype and conversion...

---
+++ 01_Basics/app4_datatype_conversion.js
@@ -0,0 +1,31 @@
+let score = "30abc"
+
+// coverting string to number
+let valueInNumber = Number(score)
+
+console.log(typeof valueInNumber)
+console.log(typeof(valueInNumber))
+console.log(valueInNumber)
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1
+// false => 0
+
+
+let isLoggedIn  = "shubham"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 0 => false
+// 1 => true
+// ""(empty string) => false
+// "  "(not empty string) or "shubham" => true
+
+
+let someNumber = 33
+
+let stringNumber  = String(someNumber);
+console.log(typeof stringNumber);
