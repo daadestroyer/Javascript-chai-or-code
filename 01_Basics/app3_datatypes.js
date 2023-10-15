7a03b562996bbbe1cd71e89598220310cff5b353
daadestroyer <nigamshubham2000@gmail.com>
2023-10-15 16:02:28 +0530

    datatype and ECMA completed

---
+++ 01_Basics/app3_datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3) // we are using nodejs not browser
+
+let name = "shubham" // string datatype
+let age = 24         // number datatype
+let isLoggedIn = false // boolean datatype
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null   => standalone value/empty value
+// undefined => means value not yet assigned
+// symbol => unique
+
+// object   
+console.log(typeof age); // number
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
