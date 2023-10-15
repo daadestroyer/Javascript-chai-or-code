6645938798c7e3a8ff17721145ebc08962276193
daadestroyer <nigamshubham2000@gmail.com>
2023-10-15 23:21:08 +0530

    datatype conversion part - 2

--- 01_Basics/app4_datatype_conversion.js
+++ 01_Basics/app4_datatype_conversion.js
@@ -29,3 +29,34 @@ let someNumber = 33
 
 let stringNumber  = String(someNumber);
 console.log(typeof stringNumber);
+
+
+// ******************* OPERATIONS ***********************
+
+
+
+let value = 3
+let negValue = -value
+console.log(negValue) // -3
+
+console.log(2+2)  // addition
+console.log(2-2)  // subtraction
+console.log(2*2)  // multiplication
+console.log(6/3)  // division (gives quotient)
+console.log(6%3)  // modulus (gives remainder)
+console.log(2**2) // 2 raise to the power 2 = 4
+console.log(2**3) // 2 raise to the power 3 = 8
+
+
+console.log();
+
+let str1 = "hello"
+let str2 = " shubham"
+let str3 = str1+str2
+console.log(str3);
+
+// execution flow is going from left to right
+console.log("1"+2); // 12
+console.log(1+"2"); // 12
+console.log("1"+2+2); // 122 (execution going from left to right hence first comes string 1 then 2 concat with 1 and then again 2 concat with 2)
+console.log(1+2+"2"); // 32 (execution going from left to right hence 1+2 = 3 of type number that will later on concat with 2)
